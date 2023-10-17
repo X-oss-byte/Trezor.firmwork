trezor/trezor-firmware

u5 - basic support

core/embed/trezorhal/stm32u5/platform.c
@@ -1,0 +1,192 @@
+/*
+ * This file is part of the Trezor project, https://trezor.io/
+ *
+ * Copyright (c) SatoshiLabs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include STM32_HAL_H
+
+#include "platform.h"
+#include "rng.h"
+
+const uint8_t AHBPrescTable[16] = {0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U,
+                                   1U, 2U, 3U, 4U, 6U, 7U, 8U, 9U};
+const uint8_t APBPrescTable[8] = {0U, 0U, 0U, 0U, 1U, 2U, 3U, 4U};
+const uint32_t MSIRangeTable[16] = {48000000U, 24000000U, 16000000U, 12000000U,
+                                    4000000U,  2000000U,  1330000U,  1000000U,
+                                    3072000U,  1536000U,  1024000U,  768000U,
+                                    400000U,   200000U,   133000U,   100000U};
+typedef struct {
+  uint32_t freq;
+  uint32_t pllq;
+  uint32_t pllp;
+  uint32_t pllm;
+  uint32_t plln;
+} clock_conf_t;
+
+#if defined STM32U5A9xx
+// HSE 16MHz
+#define DEFAULT_FREQ 160U
+#define DEFAULT_PLLM 1
+#define DEFAULT_PLLN 10  // mult by 10
+#define DEFAULT_PLLR 1U  // division by 1
+#define DEFAULT_PLLQ 1U  // division by 1
+#define DEFAULT_PLLP 5U  // division by 5
+#else
+#error Unsupported MCU
+#endif
+
+uint32_t SystemCoreClock = DEFAULT_FREQ * 1000000U;
+
+// PLLCLK = ((HSE / PLLM) * PLLN) / PLLR
+
+// assuming HSE 16 MHz
+clock_conf_t clock_conf[1] = {
+    {
+        // clk = ((16MHz / 1) * 10) / 1 = 160 MHz
+        .freq = 160,
+        .pllp = 1,
+        .pllq = 1,
+        .pllm = 1,
+        .plln = 10,
+    },
+};
+
+#pragma GCC optimize( \
+    "no-stack-protector")  // applies to all functions in this file
+
+void SystemInit(void) {
+  // set flash wait states for an increasing HCLK frequency
+
+  FLASH->ACR = FLASH_ACR_LATENCY_5WS;
+  // wait until the new wait state config takes effect -- per section 3.5.1
+  // guidance
+  while ((FLASH->ACR & FLASH_ACR_LATENCY) != FLASH_ACR_LATENCY_5WS)
+    ;
+
+  /* Reset the RCC clock configuration to the default reset state ------------*/
+  /* Set MSION bit */
+  RCC->CR = RCC_CR_MSISON;
+
+  /* Reset CFGR register */
+  RCC->CFGR1 = 0U;
+  RCC->CFGR2 = 0U;
+  RCC->CFGR3 = 0U;
+
+  /* Reset HSEON, CSSON , HSION, PLLxON bits */
+  RCC->CR &= ~(RCC_CR_HSEON | RCC_CR_CSSON | RCC_CR_PLL1ON | RCC_CR_PLL2ON |
+               RCC_CR_PLL3ON | RCC_CR_HSI48ON);
+
+  /* Reset PLLCFGR register */
+  RCC->PLL1CFGR = 0U;
+
+  /* Reset HSEBYP bit */
+  RCC->CR &= ~(RCC_CR_HSEBYP);
+
+  /* Disable all interrupts */
+  RCC->CIER = 0U;
+
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  MODIFY_REG(PWR->VOSR, (PWR_VOSR_VOS | PWR_VOSR_BOOSTEN),
+             PWR_REGULATOR_VOLTAGE_SCALE1);
+  while (HAL_IS_BIT_CLR(PWR->VOSR, PWR_VOSR_VOSRDY))
+    ;
+  while (HAL_IS_BIT_CLR(PWR->SVMSR, PWR_SVMSR_ACTVOSRDY))
+    ;
+
+  __HAL_RCC_HSE_CONFIG(RCC_HSE_ON);
+  while (READ_BIT(RCC->CR, RCC_CR_HSERDY) == 0U)
+    ;
+
+  __HAL_RCC_PLL_CONFIG(RCC_PLLSOURCE_HSE, RCC_PLLMBOOST_DIV1, DEFAULT_PLLM,
+                       DEFAULT_PLLN, DEFAULT_PLLP, DEFAULT_PLLQ, DEFAULT_PLLR);
+
+  __HAL_RCC_PLL_FRACN_DISABLE();
+
+  __HAL_RCC_PLL_VCIRANGE(RCC_PLLVCIRANGE_0);
+
+  __HAL_RCC_PLLCLKOUT_ENABLE(RCC_PLL1_DIVR);
+
+  __HAL_RCC_PLL_ENABLE();
+  while (READ_BIT(RCC->CR, RCC_CR_PLL1RDY) == 0U)
+    ;
+
+  __HAL_RCC_HSI48_ENABLE();
+  while (READ_BIT(RCC->CR, RCC_CR_HSI48RDY) == 0U)
+    ;
+
+  /** Initializes the CPU, AHB and APB buses clocks
+   */
+  __HAL_FLASH_SET_LATENCY(FLASH_LATENCY_4);
+  MODIFY_REG(RCC->CFGR3, RCC_CFGR3_PPRE3, RCC_HCLK_DIV1);
+  MODIFY_REG(RCC->CFGR2, RCC_CFGR2_PPRE2, ((RCC_HCLK_DIV1) << 4));
+  MODIFY_REG(RCC->CFGR2, RCC_CFGR2_PPRE1, RCC_HCLK_DIV1);
+  MODIFY_REG(RCC->CFGR2, RCC_CFGR2_HPRE, RCC_SYSCLK_DIV1);
+
+  MODIFY_REG(RCC->CFGR1, RCC_CFGR1_SW, RCC_SYSCLKSOURCE_PLLCLK);
+
+  /*
+   * Disable the internal Pull-Up in Dead Battery pins of UCPD peripheral
+   */
+  HAL_PWREx_DisableUCPDDeadBattery();
+
+  /*
+   * Switch to SMPS regulator instead of LDO
+   */
+  SET_BIT(PWR->CR3, PWR_CR3_REGSEL);
+  /* Wait until system switch on new regulator */
+  while (HAL_IS_BIT_CLR(PWR->SVMSR, PWR_SVMSR_REGS))
+    ;
+
+  __HAL_RCC_PWR_CLK_DISABLE();
+
+  // this will be overriden by static initialization
+  SystemCoreClock = DEFAULT_FREQ * 1000000U;
+
+  // enable clock security system, HSE clock, and main PLL
+  RCC->CR |= RCC_CR_CSSON;
+
+  // turn off the HSI as it is now unused (it will be turned on again
+  // automatically if a clock security failure occurs)
+  RCC->CR &= ~RCC_CR_HSION;
+  // wait until ths HSI is off
+  while ((RCC->CR & RCC_CR_HSION) == RCC_CR_HSION)
+    ;
+
+  // TODO turn off MSI?
+
+  // init the TRNG peripheral
+  rng_init();
+
+  // set CP10 and CP11 to enable full access to the fpu coprocessor;
+  SCB->CPACR |= ((3U << 22) | (3U << 20));
+}
+
+void drop_privileges(void) {
+  // jump to unprivileged mode
+  // http://infocenter.arm.com/help/topic/com.arm.doc.dui0552a/CHDBIBGJ.html
+  __asm__ volatile("msr control, %0" ::"r"(0x1));
+  __asm__ volatile("isb");
+}
+
+// from util.s
+extern void shutdown_privileged(void);
+
+void PVD_PVM_IRQHandler(void) {
+  TIM1->CCR1 = 0;  // turn off display backlight
+  shutdown_privileged();
+}

core/embed/trezorhal/stm32u5/util.s
@@ -1,0 +1,177 @@
+  .syntax unified
+
+  .text
+
+  .global memset_reg
+  .type memset_reg, STT_FUNC
+memset_reg:
+  // call with the following (note that the arguments are not validated prior to use):
+  // r0 - address of first word to write (inclusive)
+  // r1 - address of first word following the address in r0 to NOT write (exclusive)
+  // r2 - word value to be written
+  // both addresses in r0 and r1 needs to be divisible by 4!
+  .L_loop_begin:
+    str r2, [r0], 4 // store the word in r2 to the address in r0, post-indexed
+    cmp r0, r1
+  bne .L_loop_begin
+  bx lr
+
+  // Jump to address given in first argument R0 that points to next's stage's VTOR
+  // Clear memory and all registers before jump
+  .global jump_to
+  .type jump_to, STT_FUNC
+jump_to:
+  ldr r1, =0
+  bl jump_to_with_flag
+
+  // Jump to address given in first argument R0 that points to next's stage's VTOR
+  // Clear memory and all registers before jump. Second argument R1 is copied to R11
+  // and kept after jump.
+  .global jump_to_with_flag
+  .type jump_to_with_flag, STT_FUNC
+jump_to_with_flag:
+  mov r4, r0            // save input argument r0 (the address of the next stage's vector table) (r4 is callee save)
+  mov r11, r1           // save second argument in "flag" register because we'll be cleaning RAM
+  // this subroutine re-points the exception handlers before the C code
+  // that comprises them has been given a good environment to run.
+  // therefore, this code needs to disable interrupts before the VTOR
+  // update. then, the reset_handler of the next stage needs to re-enable interrupts.
+  // the following prevents activation of all exceptions except Non-Maskable Interrupt (NMI).
+  // according to "ARM Cortex-M Programming Guide to Memory Barrier Instructions" Application Note 321, section 4.8:
+  // "there is no requirement to insert memory barrier instructions after CPSID".
+  cpsid f
+  // wipe memory at the end of the current stage of code
+  ldr r2, =0             // r2 - the word-sized value to be written
+  ldr r0, =sram1_start   // r0 - point to beginning of SRAM
+  ldr r1, =sram1_end     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+  ldr r0, =sram2_start   // r0 - point to beginning of SRAM
+  ldr r1, =firmware_header_start     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+  ldr r0, =sram4_start   // r0 - point to beginning of SRAM
+  ldr r1, =sram4_end     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+
+  mov lr, r4
+  // clear out the general purpose registers before the next stage's except the register with flag R11
+  ldr r0, =0
+  mov r1, r0
+  mov r2, r0
+  mov r3, r0
+  mov r4, r0
+  mov r5, r0
+  mov r6, r0
+  mov r7, r0
+  mov r8, r0
+  mov r9, r0
+  mov r10, r0
+  mov r12, r0
+  // give the next stage a fresh main stack pointer
+  ldr r0, [lr]          // set r0 to the main stack pointer in the next stage's vector table
+  msr msp, r0           // give the next stage its main stack pointer
+  // point to the next stage's exception handlers
+  // AN321, section 4.11: "a memory barrier is not required after a VTOR update"
+  .set SCB_VTOR, 0xE000ED08 // reference "Cortex-M4 Devices Generic User Guide" section 4.3
+  ldr r0, =SCB_VTOR
+  str lr, [r0]
+  mov r0, r1            // zero out r0
+  // go on to the next stage
+  ldr lr, [lr, 4]       // set lr to the next stage's reset_handler
+  bx lr
+
+  .global jump_to_unprivileged
+  .type jump_to_unprivileged, STT_FUNC
+jump_to_unprivileged:
+  mov r4, r0            // save input argument r0 (the address of the next stage's vector table) (r4 is callee save)
+  // this subroutine re-points the exception handlers before the C code
+  // that comprises them has been given a good environment to run.
+  // therefore, this code needs to disable interrupts before the VTOR
+  // update. then, the reset_handler of the next stage needs to re-enable interrupts.
+  // the following prevents activation of all exceptions except Non-Maskable Interrupt (NMI).
+  // according to "ARM Cortex-M Programming Guide to Memory Barrier Instructions" Application Note 321, section 4.8:
+  // "there is no requirement to insert memory barrier instructions after CPSID".
+  cpsid f
+  // wipe memory at the end of the current stage of code
+  ldr r2, =0             // r2 - the word-sized value to be written
+  ldr r0, =sram1_start   // r0 - point to beginning of SRAM
+  ldr r1, =sram1_end     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+  ldr r0, =sram2_start   // r0 - point to beginning of SRAM
+  ldr r1, =sram2_end     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+  ldr r0, =sram4_start   // r0 - point to beginning of SRAM
+  ldr r1, =sram4_end     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+     // r2 - the word-sized value to be written
+  bl memset_reg
+  mov lr, r4
+  // clear out the general purpose registers before the next stage's code can run (even the NMI exception handler)
+  ldr r0, =0
+  mov r1, r0
+  mov r2, r0
+  mov r3, r0
+  mov r4, r0
+  mov r5, r0
+  mov r6, r0
+  mov r7, r0
+  mov r8, r0
+  mov r9, r0
+  mov r10, r0
+  mov r11, r0
+  mov r12, r0
+  // give the next stage a fresh main stack pointer
+  ldr r0, [lr]          // set r0 to the main stack pointer in the next stage's vector table
+  msr msp, r0           // give the next stage its main stack pointer
+  // point to the next stage's exception handlers
+  // AN321, section 4.11: "a memory barrier is not required after a VTOR update"
+  .set SCB_VTOR, 0xE000ED08 // reference "Cortex-M4 Devices Generic User Guide" section 4.3
+  ldr r0, =SCB_VTOR
+  str lr, [r0]
+  mov r0, r1            // zero out r0
+  // go on to the next stage
+  ldr lr, [lr, 4]       // set lr to the next stage's reset_handler
+  // switch to unprivileged mode
+  ldr r0, =1
+  msr control, r0
+  isb
+  // jump
+  bx lr
+
+  .global shutdown_privileged
+  .type shutdown_privileged, STT_FUNC
+  // The function must be called from the privileged mode
+shutdown_privileged:
+  cpsid f // disable all exceptions (except for NMI), the instruction is ignored in unprivileged mode
+  // if the exceptions weren't disabled, an exception handler (for example systick handler)
+  // could be called after the memory is erased, which would lead to another exception
+  ldr r0, =0
+  mov r1, r0
+  mov r2, r0
+  mov r3, r0
+  mov r4, r0
+  mov r5, r0
+  mov r6, r0
+  mov r7, r0
+  mov r8, r0
+  mov r9, r0
+  mov r10, r0
+  mov r11, r0
+  mov r12, r0
+  ldr lr, =0xffffffff
+
+  ldr r0, =sram1_start   // r0 - point to beginning of SRAM
+  ldr r1, =sram1_end     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+  ldr r0, =sram2_start   // r0 - point to beginning of SRAM
+  ldr r1, =sram2_end     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+  ldr r0, =sram4_start   // r0 - point to beginning of SRAM
+  ldr r1, =sram4_end     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+
+  ldr r0, =1
+  msr control, r0 // jump to unprivileged mode
+  ldr r0, =0
+  b . // loop forever
+
+  .end

X-oss-byte/Trezor.firmwork

core/embed/trezorhal/stm32u5/platform.c
@@ -1,0 +1,205 @@
+/*
+ * This file is part of the Trezor project, https://trezor.io/
+ *
+ * Copyright (c) SatoshiLabs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include STM32_HAL_H
+
+#include "platform.h"
+#include "rng.h"
+
+const uint8_t AHBPrescTable[16] = {0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U,
+                                   1U, 2U, 3U, 4U, 6U, 7U, 8U, 9U};
+const uint8_t APBPrescTable[8] = {0U, 0U, 0U, 0U, 1U, 2U, 3U, 4U};
+const uint32_t MSIRangeTable[16] = {48000000U, 24000000U, 16000000U, 12000000U,
+                                    4000000U,  2000000U,  1330000U,  1000000U,
+                                    3072000U,  1536000U,  1024000U,  768000U,
+                                    400000U,   200000U,   133000U,   100000U};
+typedef struct {
+  uint32_t freq;
+  uint32_t pllq;
+  uint32_t pllp;
+  uint32_t pllm;
+  uint32_t plln;
+} clock_conf_t;
+
+#if defined STM32U5
+// HSE 16MHz
+#define DEFAULT_FREQ 160U
+#define DEFAULT_PLLM 1
+#define DEFAULT_PLLN 10  // mult by 10
+#define DEFAULT_PLLR 1U  // division by 1
+#define DEFAULT_PLLQ 1U  // division by 1
+#define DEFAULT_PLLP 5U  // division by 5
+#else
+#error Unsupported MCU
+#endif
+
+uint32_t SystemCoreClock = DEFAULT_FREQ * 1000000U;
+
+// PLLCLK = ((HSE / PLLM) * PLLN) / PLLR
+
+// assuming HSE 16 MHz
+clock_conf_t clock_conf[1] = {
+    {
+        // clk = ((16MHz / 1) * 10) / 1 = 160 MHz
+        .freq = 160,
+        .pllp = 1,
+        .pllq = 1,
+        .pllm = 1,
+        .plln = 10,
+    },
+};
+
+#pragma GCC optimize( \
+    "no-stack-protector")  // applies to all functions in this file
+
+void SystemInit(void) {
+  // set flash wait states for an increasing HCLK frequency
+
+  FLASH->ACR = FLASH_ACR_LATENCY_5WS;
+  // wait until the new wait state config takes effect -- per section 3.5.1
+  // guidance
+  while ((FLASH->ACR & FLASH_ACR_LATENCY) != FLASH_ACR_LATENCY_5WS)
+    ;
+
+  /* Reset the RCC clock configuration to the default reset state ------------*/
+  /* Set MSION bit */
+  RCC->CR = RCC_CR_MSISON;
+
+  /* Reset CFGR register */
+  RCC->CFGR1 = 0U;
+  RCC->CFGR2 = 0U;
+  RCC->CFGR3 = 0U;
+
+  /* Reset HSEON, CSSON , HSION, PLLxON bits */
+  RCC->CR &= ~(RCC_CR_HSEON | RCC_CR_CSSON | RCC_CR_PLL1ON | RCC_CR_PLL2ON |
+               RCC_CR_PLL3ON | RCC_CR_HSI48ON);
+
+  /* Reset PLLCFGR register */
+  RCC->PLL1CFGR = 0U;
+
+  /* Reset HSEBYP bit */
+  RCC->CR &= ~(RCC_CR_HSEBYP);
+
+  /* Disable all interrupts */
+  RCC->CIER = 0U;
+
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  MODIFY_REG(PWR->VOSR, (PWR_VOSR_VOS | PWR_VOSR_BOOSTEN),
+             PWR_REGULATOR_VOLTAGE_SCALE1);
+  while (HAL_IS_BIT_CLR(PWR->VOSR, PWR_VOSR_VOSRDY))
+    ;
+  while (HAL_IS_BIT_CLR(PWR->SVMSR, PWR_SVMSR_ACTVOSRDY))
+    ;
+
+  __HAL_RCC_HSE_CONFIG(RCC_HSE_ON);
+  while (READ_BIT(RCC->CR, RCC_CR_HSERDY) == 0U)
+    ;
+
+  __HAL_RCC_PLL_CONFIG(RCC_PLLSOURCE_HSE, RCC_PLLMBOOST_DIV1, DEFAULT_PLLM,
+                       DEFAULT_PLLN, DEFAULT_PLLP, DEFAULT_PLLQ, DEFAULT_PLLR);
+
+  __HAL_RCC_PLL_FRACN_DISABLE();
+
+  __HAL_RCC_PLL_VCIRANGE(RCC_PLLVCIRANGE_1);
+
+  __HAL_RCC_PLLCLKOUT_ENABLE(RCC_PLL1_DIVR);
+
+  __HAL_RCC_PLL_ENABLE();
+  while (READ_BIT(RCC->CR, RCC_CR_PLL1RDY) == 0U)
+    ;
+
+  __HAL_RCC_HSI48_ENABLE();
+  while (READ_BIT(RCC->CR, RCC_CR_HSI48RDY) == 0U)
+    ;
+
+  /** Initializes the CPU, AHB and APB buses clocks
+   */
+  FLASH->ACR = FLASH_ACR_LATENCY_4WS;
+  // wait until the new wait state config takes effect -- per section 3.5.1
+  // guidance
+  while ((FLASH->ACR & FLASH_ACR_LATENCY) != FLASH_ACR_LATENCY_4WS)
+    ;
+  MODIFY_REG(RCC->CFGR3, RCC_CFGR3_PPRE3, RCC_HCLK_DIV1);
+  MODIFY_REG(RCC->CFGR2, RCC_CFGR2_PPRE2, ((RCC_HCLK_DIV1) << 4));
+  MODIFY_REG(RCC->CFGR2, RCC_CFGR2_PPRE1, RCC_HCLK_DIV1);
+  MODIFY_REG(RCC->CFGR2, RCC_CFGR2_HPRE, RCC_SYSCLK_DIV1);
+
+  MODIFY_REG(RCC->CFGR1, RCC_CFGR1_SW, RCC_SYSCLKSOURCE_PLLCLK);
+
+  /*
+   * Disable the internal Pull-Up in Dead Battery pins of UCPD peripheral
+   */
+  HAL_PWREx_DisableUCPDDeadBattery();
+
+  /*
+   * Switch to SMPS regulator instead of LDO
+   */
+  SET_BIT(PWR->CR3, PWR_CR3_REGSEL);
+  /* Wait until system switch on new regulator */
+  while (HAL_IS_BIT_CLR(PWR->SVMSR, PWR_SVMSR_REGS))
+    ;
+
+  __HAL_RCC_PWR_CLK_DISABLE();
+
+  // this will be overriden by static initialization
+  SystemCoreClock = DEFAULT_FREQ * 1000000U;
+
+  // enable clock security system, HSE clock, and main PLL
+  RCC->CR |= RCC_CR_CSSON;
+
+  // turn off the HSI as it is now unused (it will be turned on again
+  // automatically if a clock security failure occurs)
+  RCC->CR &= ~RCC_CR_HSION;
+  // wait until ths HSI is off
+  while ((RCC->CR & RCC_CR_HSION) == RCC_CR_HSION)
+    ;
+
+  // TODO turn off MSI?
+
+  // init the TRNG peripheral
+  rng_init();
+
+  // set CP10 and CP11 to enable full access to the fpu coprocessor;
+  SCB->CPACR |=
+      ((3UL << 20U) | (3UL << 22U)); /* set CP10 and CP11 Full Access */
+
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U)
+  SCB_NS->CPACR |=
+      ((3UL << 20U) | (3UL << 22U)); /* set CP10 and CP11 Full Access */
+#endif
+
+  // enable instruction cache
+  ICACHE->CR = ICACHE_CR_EN;
+}
+
+void drop_privileges(void) {
+  // jump to unprivileged mode
+  // http://infocenter.arm.com/help/topic/com.arm.doc.dui0552a/CHDBIBGJ.html
+  __asm__ volatile("msr control, %0" ::"r"(0x1));
+  __asm__ volatile("isb");
+}
+
+// from util.s
+extern void shutdown_privileged(void);
+
+void PVD_PVM_IRQHandler(void) {
+  TIM1->CCR1 = 0;  // turn off display backlight
+  shutdown_privileged();
+}

core/embed/trezorhal/stm32u5/util.s
@@ -1,0 +1,177 @@
+  .syntax unified
+
+  .text
+
+  .global memset_reg
+  .type memset_reg, STT_FUNC
+memset_reg:
+  // call with the following (note that the arguments are not validated prior to use):
+  // r0 - address of first word to write (inclusive)
+  // r1 - address of first word following the address in r0 to NOT write (exclusive)
+  // r2 - word value to be written
+  // both addresses in r0 and r1 needs to be divisible by 4!
+  .L_loop_begin:
+    str r2, [r0], 4 // store the word in r2 to the address in r0, post-indexed
+    cmp r0, r1
+  bne .L_loop_begin
+  bx lr
+
+  // Jump to address given in first argument R0 that points to next's stage's VTOR
+  // Clear memory and all registers before jump
+  .global jump_to
+  .type jump_to, STT_FUNC
+jump_to:
+  ldr r1, =0
+  bl jump_to_with_flag
+
+  // Jump to address given in first argument R0 that points to next's stage's VTOR
+  // Clear memory and all registers before jump. Second argument R1 is copied to R11
+  // and kept after jump.
+  .global jump_to_with_flag
+  .type jump_to_with_flag, STT_FUNC
+jump_to_with_flag:
+  mov r4, r0            // save input argument r0 (the address of the next stage's vector table) (r4 is callee save)
+  mov r11, r1           // save second argument in "flag" register because we'll be cleaning RAM
+  // this subroutine re-points the exception handlers before the C code
+  // that comprises them has been given a good environment to run.
+  // therefore, this code needs to disable interrupts before the VTOR
+  // update. then, the reset_handler of the next stage needs to re-enable interrupts.
+  // the following prevents activation of all exceptions except Non-Maskable Interrupt (NMI).
+  // according to "ARM Cortex-M Programming Guide to Memory Barrier Instructions" Application Note 321, section 4.8:
+  // "there is no requirement to insert memory barrier instructions after CPSID".
+  cpsid f
+  // wipe memory at the end of the current stage of code
+  ldr r2, =0             // r2 - the word-sized value to be written
+  ldr r0, =sram1_start   // r0 - point to beginning of SRAM
+  ldr r1, =_stay_in_bootloader_flag_addr     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+  ldr r0, =sram2_start   // r0 - point to beginning of SRAM
+  ldr r1, =sram2_end     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+  ldr r0, =sram4_start   // r0 - point to beginning of SRAM
+  ldr r1, =sram4_end     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+
+  mov lr, r4
+  // clear out the general purpose registers before the next stage's except the register with flag R11
+  ldr r0, =0
+  mov r1, r0
+  mov r2, r0
+  mov r3, r0
+  mov r4, r0
+  mov r5, r0
+  mov r6, r0
+  mov r7, r0
+  mov r8, r0
+  mov r9, r0
+  mov r10, r0
+  mov r12, r0
+  // give the next stage a fresh main stack pointer
+  ldr r0, [lr]          // set r0 to the main stack pointer in the next stage's vector table
+  msr msp, r0           // give the next stage its main stack pointer
+  // point to the next stage's exception handlers
+  // AN321, section 4.11: "a memory barrier is not required after a VTOR update"
+  .set SCB_VTOR, 0xE000ED08 // reference "Cortex-M4 Devices Generic User Guide" section 4.3
+  ldr r0, =SCB_VTOR
+  str lr, [r0]
+  mov r0, r1            // zero out r0
+  // go on to the next stage
+  ldr lr, [lr, 4]       // set lr to the next stage's reset_handler
+  bx lr
+
+  .global jump_to_unprivileged
+  .type jump_to_unprivileged, STT_FUNC
+jump_to_unprivileged:
+  mov r4, r0            // save input argument r0 (the address of the next stage's vector table) (r4 is callee save)
+  // this subroutine re-points the exception handlers before the C code
+  // that comprises them has been given a good environment to run.
+  // therefore, this code needs to disable interrupts before the VTOR
+  // update. then, the reset_handler of the next stage needs to re-enable interrupts.
+  // the following prevents activation of all exceptions except Non-Maskable Interrupt (NMI).
+  // according to "ARM Cortex-M Programming Guide to Memory Barrier Instructions" Application Note 321, section 4.8:
+  // "there is no requirement to insert memory barrier instructions after CPSID".
+  cpsid f
+  // wipe memory at the end of the current stage of code
+  ldr r2, =0             // r2 - the word-sized value to be written
+  ldr r0, =sram1_start   // r0 - point to beginning of SRAM
+  ldr r1, =sram1_end     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+  ldr r0, =sram2_start   // r0 - point to beginning of SRAM
+  ldr r1, =sram2_end     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+  ldr r0, =sram4_start   // r0 - point to beginning of SRAM
+  ldr r1, =sram4_end     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+     // r2 - the word-sized value to be written
+  bl memset_reg
+  mov lr, r4
+  // clear out the general purpose registers before the next stage's code can run (even the NMI exception handler)
+  ldr r0, =0
+  mov r1, r0
+  mov r2, r0
+  mov r3, r0
+  mov r4, r0
+  mov r5, r0
+  mov r6, r0
+  mov r7, r0
+  mov r8, r0
+  mov r9, r0
+  mov r10, r0
+  mov r11, r0
+  mov r12, r0
+  // give the next stage a fresh main stack pointer
+  ldr r0, [lr]          // set r0 to the main stack pointer in the next stage's vector table
+  msr msp, r0           // give the next stage its main stack pointer
+  // point to the next stage's exception handlers
+  // AN321, section 4.11: "a memory barrier is not required after a VTOR update"
+  .set SCB_VTOR, 0xE000ED08 // reference "Cortex-M4 Devices Generic User Guide" section 4.3
+  ldr r0, =SCB_VTOR
+  str lr, [r0]
+  mov r0, r1            // zero out r0
+  // go on to the next stage
+  ldr lr, [lr, 4]       // set lr to the next stage's reset_handler
+  // switch to unprivileged mode
+  ldr r0, =1
+  msr control, r0
+  isb
+  // jump
+  bx lr
+
+  .global shutdown_privileged
+  .type shutdown_privileged, STT_FUNC
+  // The function must be called from the privileged mode
+shutdown_privileged:
+  cpsid f // disable all exceptions (except for NMI), the instruction is ignored in unprivileged mode
+  // if the exceptions weren't disabled, an exception handler (for example systick handler)
+  // could be called after the memory is erased, which would lead to another exception
+  ldr r0, =0
+  mov r1, r0
+  mov r2, r0
+  mov r3, r0
+  mov r4, r0
+  mov r5, r0
+  mov r6, r0
+  mov r7, r0
+  mov r8, r0
+  mov r9, r0
+  mov r10, r0
+  mov r11, r0
+  mov r12, r0
+  ldr lr, =0xffffffff
+
+  ldr r0, =sram1_start   // r0 - point to beginning of SRAM
+  ldr r1, =sram1_end     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+  ldr r0, =sram2_start   // r0 - point to beginning of SRAM
+  ldr r1, =sram2_end     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+  ldr r0, =sram4_start   // r0 - point to beginning of SRAM
+  ldr r1, =sram4_end     // r1 - point to byte after the end of SRAM
+  bl memset_reg
+
+  ldr r0, =1
+  msr control, r0 // jump to unprivileged mode
+  ldr r0, =0
+  b . // loop forever
+
+  .end